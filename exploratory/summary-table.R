bd28f50ab784c4d96c328211e9c1548039c01b80
navyag09 <navyag09@uw.edu>
2021-11-23 00:04:39 -0800

    fixed

--- exploratory/summary-table.R
+++ exploratory/summary-table.R
@@ -3,6 +3,8 @@
 library(data.table)
 library(dplyr)
 
+originals <- read.csv("https://raw.githubusercontent.com/info-201a-au21/final-project-SimritaGopalan/main/data/netflix_originals.csv?token=AV5INKOEACOPFRW43Q4PN2TBTXP7K")
+
 genre_grouping <- originals %>%
   group_by(Genre) %>%
   summarize(mean_scores = mean(IMDB.Score))
@@ -17,3 +19,4 @@ avg_scores_table$V2 <- round(avg_scores_table$V2, digit = 2)
 avg_scores_table$V3 <- num_genre[, 2]
 colnames(avg_scores_table) <- c("Genre", "Average IMDB Score", "Count")
 avg_scores_table <- avg_scores_table[, c(1, 3, 2)]
+
